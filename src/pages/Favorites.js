7d2de1300cf967825fdffc9ae5c4a96f56e4ccd3
Ana-Maria Rotariu <anamariarotariu5@gmail.com>
2022-02-02 14:44:44 +0200

    favorite page done

--- src/pages/Favorites.js
+++ src/pages/Favorites.js
@@ -1,4 +1,18 @@
+import { useContext } from "react";
+import FavoritesContext from "../store/favorites-context";
+import MeetupList from "../components/meetups/MeetupList";
 function FavoritesPage() {
-    return <div>Favorites page</div>
+    const favoriteCont = useContext(FavoritesContext);
+    let content;
+    if (favoriteCont.totalFavorites === 0) {
+        content = <p>You got no favorites yet.</p>
+    } else {
+        content = <MeetupList meetups={favoriteCont.favorites}></MeetupList>
+    }
+
+    return <section>
+        <h1> My Favorites</h1>
+        {content}
+    </section>
 }
 export default FavoritesPage;
